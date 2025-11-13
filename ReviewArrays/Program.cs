592c31ee6b095d307dd9bd6a82bf0b7537aaa966
GABRIELLA SANTOS LIMA <52303883822@info.local>
2025-11-13 10:53:00 -0300

    Adicionado exercicios de POO

---
+++ ReviewArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿//Arrays
+
+string nome = "Gabriella";
+Console.WriteLine($"Nome: {nome}");
+string[] nomes = new string [4];
+
+nomes [0] = "Maria";
+nomes [1] = "Eloysa";
+nomes [2] = "Matheus";
+nomes [3] = "Livia";
+nomes [4] = "Amy lee";
+
+Console.WriteLine();
+
+for (int i = 0; i < nomes.Length; i++)
+{
+Console.WriteLine();
+}
